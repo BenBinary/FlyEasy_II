ce424218cbb62d2c63fd89e42a0d2ee5067a2234
Randy Tasajaya <RandyTasajaya@users.noreply.github.com>
2019-05-18 13:51:31 +0200

    Create Adresse.java

---
+++ FlyEasy/src/fachlogik/Adresse.java
@@ -0,0 +1,12 @@
+public class Adresse {
+
+    String strasse;
+    String stadt;
+
+    public Adresse(){}
+
+    public Adresse(String strasse, String stadt) {
+        this.strasse = strasse;
+        this.stadt = stadt;
+    }
+}
